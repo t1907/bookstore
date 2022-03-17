168136ef24858395000dda8e3b60fc62e3f953f2
tomasz3bb <73695370+tomasz3bb@users.noreply.github.com>
2022-03-17 09:53:10 +0100

    start

--- src/main/java/pl/project/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/pl/project/bookstore/service/impl/BookServiceImpl.java
@@ -1,6 +1,7 @@
 package pl.project.bookstore.service.impl;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.project.bookstore.model.Book;
 import pl.project.bookstore.repository.BookRepository;
